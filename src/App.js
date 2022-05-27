dfaf0f5c095cc9299437b5b0c9c58400ec7ec021
DongHun, Kim <donghun.kdh@gmail.com>
2022-05-28 03:04:46 +0900

    CHORE: rename classname

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import data from './data.js';
 
 function App() {
+  let [books] = useState(data);
   return (
     <div className='App'>
       <Navbar bg='dark' variant='dark'>
@@ -17,39 +18,41 @@ function App() {
         </Container>
       </Navbar>
       <div className='main-banner'></div>
-      <Container className='product-container'>
-        <Row className='product-row'>
+      <Container className='book-container'>
+        <Row className='book-row'>
           <Col sm>
             <img
               src='http://image.yes24.com/goods/109711067/XL'
               alt=''
-              className='product-img'
+              className='book-img'
             />
-            <h4 className='product-name'>상품명</h4>
-            <p className='product-description'>상품설명</p>
+            <h4 className='book-name'>상품명</h4>
+            <p className='book-description'>상품설명</p>
           </Col>
           <Col sm>
             <img
               src='http://image.yes24.com/goods/91433923/XL'
               alt=''
-              className='product-img'
+              className='book-img'
             />
-            <h4 className='product-name'>상품명</h4>
-            <p className='product-description'>상품설명</p>
+            <h4 className='book-name'>상품명</h4>
+            <p className='book-description'>상품설명</p>
           </Col>
           <Col sm>
             <img
               src='http://image.yes24.com/goods/107490270/XL'
               alt=''
-              className='product-img'
+              className='book-img'
             />
-            <h4 className='product-name'>상품명</h4>
-            <p className='product-description'>상품설명</p>
+            <h4 className='book-name'>상품명</h4>
+            <p className='book-description'>상품설명</p>
           </Col>
         </Row>
       </Container>
     </div>
   );
 }
+
+function Book() {}
 
 export default App;
