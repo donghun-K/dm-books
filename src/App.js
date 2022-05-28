59988c998d38b3fcd60001e22108ab14b88da2f1
DongHun, Kim <donghun.kdh@gmail.com>
2022-05-28 19:00:04 +0900

    REFACTOR: use useNavigate instead Link

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import './App.css';
 import { Row, Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail.js';
 import Card from './components/Card.js';
 
 function App() {
   let [books] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className='App'>
       <Navbar bg='dark' variant='dark'>
         <Container>
           <Navbar.Brand href='#home'>DM BOOKS</Navbar.Brand>
           <Nav className='me-auto'>
-            <Link to='/'>홈</Link>
-            <Link to='/detail'>상세페이지</Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/detail');
+              }}
+            >
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
