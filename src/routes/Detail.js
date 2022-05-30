198cc18a74283cd137decbcb95e0a3a3155718e1
DongHun, Kim <donghun.kdh@gmail.com>
2022-05-31 06:01:07 +0900

    TEST: test useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react';
 import { Col, Row, Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
+  let [num, setNum] = useState('');
+  useEffect(() => {
+    if (isNaN(num) === true) {
+      alert('글자 ㄴ');
+    }
+  }, [num]);
   let { id } = useParams();
   let book = props.books.find((book) => {
     return book.id === Number(id);
@@ -13,6 +20,12 @@ function Detail(props) {
           <img src={book.src} width='100%' alt='book-img' />
         </Col>
         <Col md='6'>
+          <input
+            type='text'
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
           <h4>{book.title}</h4>
           <p>{book.content}</p>
           <p>{book.price}</p>
