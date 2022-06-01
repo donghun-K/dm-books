674877b973e97783ef4995c023773c2ceaed1094
DongHun, Kim <donghun.kdh@gmail.com>
2022-06-01 10:54:17 +0900

    CHORE: remove test

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,7 @@
-import { useEffect, useState } from 'react';
 import { Col, Row, Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
-  let [num, setNum] = useState('');
-  useEffect(() => {
-    if (isNaN(num) === true) {
-      alert('글자 ㄴ');
-    }
-  }, [num]);
   let { id } = useParams();
   let book = props.books.find((book) => {
     return book.id === Number(id);
@@ -20,12 +13,6 @@ function Detail(props) {
           <img src={book.src} width='100%' alt='book-img' />
         </Col>
         <Col md='6'>
-          <input
-            type='text'
-            onChange={(e) => {
-              setNum(e.target.value);
-            }}
-          />
           <h4>{book.title}</h4>
           <p>{book.content}</p>
           <p>{book.price}</p>
