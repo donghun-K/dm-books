e89a8a58ba765dd0dcb498ceb0718ab8abce7ae2
DongHun, Kim <donghun.kdh@gmail.com>
2022-06-07 15:42:48 +0900

    DESIGN: update card component style

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,24 +4,26 @@ import { useNavigate } from 'react-router-dom';
 function Card(props) {
   let nav = useNavigate();
   return (
-    <Col sm>
-      <img
-        src={props.book['src']}
-        alt=''
-        className='book-img'
-        onClick={() => {
-          nav(`/detail/${props.id}`);
-        }}
-      />
+    <Col sm className='card-container'>
+      <div className='card-img-box'>
+        <img
+          src={props.book['src']}
+          alt=''
+          className='card-img'
+          onClick={() => {
+            nav(`/detail/${props.id}`);
+          }}
+        />
+      </div>
       <h4
-        className='book-name'
+        className='card-name'
         onClick={() => {
           nav(`/detail/${props.id}`);
         }}
       >
         {props.book['title']}
       </h4>
-      <p className='book-description'>{props.book['content']}</p>
+      <p className='card-description'>{props.book['content']}</p>
     </Col>
   );
 }
