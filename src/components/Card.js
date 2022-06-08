c23f367dca5a06c0bfd1738c880148143d4ef87d
DongHun, Kim <donghun.kdh@gmail.com>
2022-06-09 06:28:37 +0900

    DESIGN: modify Card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,26 +4,28 @@ import { useNavigate } from 'react-router-dom';
 function Card(props) {
   let nav = useNavigate();
   return (
-    <Col sm className='card-container'>
-      <div className='card-img-box'>
-        <img
-          src={props.book['src']}
-          alt=''
-          className='card-img'
+    <Col sm>
+      <div className='card-container'>
+        <div className='card-img-box'>
+          <img
+            src={props.book['src']}
+            alt=''
+            className='card-img'
+            onClick={() => {
+              nav(`/detail/${props.id}`);
+            }}
+          />
+        </div>
+        <h4
+          className='card-name'
           onClick={() => {
             nav(`/detail/${props.id}`);
           }}
-        />
+        >
+          {props.book['title']}
+        </h4>
+        <p className='card-description'>{props.book['content']}</p>
       </div>
-      <h4
-        className='card-name'
-        onClick={() => {
-          nav(`/detail/${props.id}`);
-        }}
-      >
-        {props.book['title']}
-      </h4>
-      <p className='card-description'>{props.book['content']}</p>
     </Col>
   );
 }
